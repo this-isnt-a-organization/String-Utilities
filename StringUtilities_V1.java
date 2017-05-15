e089aa96793cd3db0ba48b7fda7da96bc829d155
dbap127 <dbapt1@ocdsb.ca>
2017-05-15 14:06:37 -0400

    Update StringUtilities_V1.java

--- StringUtilities_V1.java
+++ StringUtilities_V1.java
@@ -1,1 +1,14 @@
-
+class StringUtilities_V1
+{
+  public static void main (String []args)
+  {
+    int utilityChoice;
+    System.out.println("Welcome to Nabeel, Yassine and Denzel's String Utility Program.");
+    System.out.println("1: Count Letters");
+    System.out.println("2: Count Words");
+    System.out.println("3: Capitalize Every Word"):
+    
+    
+    
+  }
+}
