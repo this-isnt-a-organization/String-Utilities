2d0f8fd82e769dec7085cd650f9e663f250e289c
dbap127 <dbapt1@ocdsb.ca>
2017-05-15 22:26:25 -0400

    working on menu

--- StringUtilities_V1.java
+++ StringUtilities_V1.java
@@ -2,12 +2,21 @@ class StringUtilities_V1
 {
   public static void main (String []args)
   {
-    int utilityChoice;
-    System.out.println("Welcome to Nabeel, Yassine and Denzel's String Utility Program.");
+    String utilityChoice;
+    System.out.println("Welcome to Nabeel, Yassine and Denzel's String Utility Program.\n");
     System.out.println("1: Count Letters");
     System.out.println("2: Count Words");
     System.out.println("3: Capitalize Every Word");
-    
+    System.out.println("4: Reverse Statement");
+    System.out.println("5: Reverse Letters");
+    System.out.println("6: Reverse Words");
+    System.out.println("7: Alternate Case");
+    System.out.println("8: Letter Frequency");
+    System.out.println("9: Consonants vs Vowels");
+    System.out.println("10: Letters Only");
+    System.out.println("11: Search");
+    System.out.println("12: Search & Replace");
+    System.out.println("\nIf you want descriptions of these utilities, input \"des NUMBER\" for the description of a utility.");
     
     
   }
