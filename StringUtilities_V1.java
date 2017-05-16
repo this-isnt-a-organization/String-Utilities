a4fb6bdfe35b6c440dc024cb7cabe4db5b5f3561
dbap127 <dbapt1@ocdsb.ca>
2017-05-16 14:52:53 -0400

    placed all descriptions in another method

--- StringUtilities_V1.java
+++ StringUtilities_V1.java
@@ -38,58 +38,7 @@ class StringUtilities_V1
       String[] commandFind = utilityChoice.split("\\s+");
       if (commandFind[0].contains("des"))
       {
-        if (Integer.parseInt(commandFind[1]) == 1)
-        {
-          System.out.println("Count Letters\nCounts the total number of letters (no spaces, punctuation) in the statement(s).");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 2)
-        {
-          System.out.println("Count Words\nCounts the total number of words in the statement(s).");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 3)
-        {
-          System.out.println("Capitalize Every Word\nChanges the first letter of each word to uppercase.");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 4)
-        {
-          System.out.println("Reverse Statement\nOutputs the statement(s) completely in reverse. The first character is now last, the last character is now first.");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 5)
-        {
-          System.out.println("Reverse Letters\nOutputs the statement(s) so the words are in the correct order, but the letters from each word are reversed.");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 6)
-        {
-          System.out.println("Reverse Words\nOutputs the statement(s) so the words are in the correct order, but the letters from each word are still in the correct order.");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 7)
-        {
-          System.out.println("Alternate Case\nOutputs the statement so the first letter is uppercase, the 2nd is lowercase, the 3rd is uppercase, etc.");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 8)
-        {
-          System.out.println("Letter Frequency\nAnalyzes the statement(s) to determine which letters are present, and outputs a summary of the count for each letter that's present.");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 9)
-        {
-          System.out.println("Consonants vs Vowels\nAnalyses the statement(s) to determine the number of consonants and vowels in the word and determines the number of spaces and punctuation.");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 10)
-        {
-          System.out.println("Letters Only\nDisplays the statement(s) with no spaces or punctuation.");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 11)
-        {
-          System.out.println("Search\nFinds a letter or substring in the statement(s), then outputs the full statements indicating where the letter or substring may be found.");
-        }
-        else if (Integer.parseInt(commandFind[1]) == 12)
-        {
-          System.out.println("Search & Replace\nFinds a letter or substring and replace them with a new substring specified by the user.");
-        }
-        else
-        {
-          System.out.println("Invalid Number.");
-        }
+        descriptionShow(Integer.parseInt(commandFind[1]));
       }
       else if (commandFind[0].contains("list"))
       {
@@ -106,6 +55,62 @@ class StringUtilities_V1
       System.out.println("Invalid choice, command or number.");
     }
     
+  }
+  
+  public static void descriptionShow(int des)
+  {
+    if (des == 1)
+    {
+      System.out.println("Count Letters\nCounts the total number of letters (no spaces, punctuation) in the statement(s).");
+    }
+    else if (des == 2)
+    {
+      System.out.println("Count Words\nCounts the total number of words in the statement(s).");
+    }
+    else if (des == 3)
+    {
+      System.out.println("Capitalize Every Word\nChanges the first letter of each word to uppercase.");
+    }
+    else if (des == 4)
+    {
+      System.out.println("Reverse Statement\nOutputs the statement(s) completely in reverse. The first character is now last, the last character is now first.");
+    }
+    else if (des == 5)
+    {
+      System.out.println("Reverse Letters\nOutputs the statement(s) so the words are in the correct order, but the letters from each word are reversed.");
+    }
+    else if (des == 6)
+    {
+      System.out.println("Reverse Words\nOutputs the statement(s) so the words are in the correct order, but the letters from each word are still in the correct order.");
+    }
+    else if (des == 7)
+    {
+      System.out.println("Alternate Case\nOutputs the statement so the first letter is uppercase, the 2nd is lowercase, the 3rd is uppercase, etc.");
+    }
+    else if (des == 8)
+    {
+      System.out.println("Letter Frequency\nAnalyzes the statement(s) to determine which letters are present, and outputs a summary of the count for each letter that's present.");
+    }
+    else if (des == 9)
+    {
+      System.out.println("Consonants vs Vowels\nAnalyses the statement(s) to determine the number of consonants and vowels in the word and determines the number of spaces and punctuation.");
+    }
+    else if (des == 10)
+    {
+      System.out.println("Letters Only\nDisplays the statement(s) with no spaces or punctuation.");
+    }
+    else if (des == 11)
+    {
+      System.out.println("Search\nFinds a letter or substring in the statement(s), then outputs the full statements indicating where the letter or substring may be found.");
+    }
+    else if (des == 12)
+    {
+      System.out.println("Search & Replace\nFinds a letter or substring and replace them with a new substring specified by the user.");
+    }
+    else
+    {
+      System.out.println("Invalid Number.");
+    }
   }
   
 }
