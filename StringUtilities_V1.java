ece37caf5d8eaa7844e8093e9a85e719505acf59
ybella <ybell2@ocdsb.ca>
2017-05-17 13:12:03 -0400

    Utility #1 added
    Utility #1 added

--- StringUtilities_V1.java
+++ StringUtilities_V1.java
@@ -170,10 +170,14 @@ class StringUtilities_V1
   
   // Method to count letters in a string (excluding spaces and punctuation) (Not done, Yassine)
   public static void countLetters () {
-  
-  
-  
-  
+      String userStatement ;
+      
+      System.out.println ("Please enter a statement");
+      userStatement = In.getString ();
+      userStatement = userStatement.trim();
+      String userInput = userStatement.replaceAll("\\W+",""); // "\\W" takes out anything that is not a word character
+      // Including punctuation.
+      System.out.println ("Statement is: "+userInput.length()+" characters without spaces or punctuation.");
   }
   
   // Method to count words in a string (Not done, Denzel)
